e71e32ee3bed4ce6be96a770d7f6fe6eeed940a7
Jack Tomlinson <jtommo66@hotmail.com>
2024-02-22 12:32:50 +0000

    add app structure with header set up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.scss";
+import background from "./images/snookerbackground.jpg";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div
+      className="App"
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    ></div>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,5 @@
+import "./Header.scss";
+
+function Header() {
+  return <h1>This is the header</h1>;
+}
